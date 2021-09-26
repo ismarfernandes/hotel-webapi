a2575a46f586accae3afe3d6688660e9af138dae
Ismar Rafael Fernandes <ismarfernandes@outlook.com>
2021-09-26 15:00:43 -0300

    fix: add a parameterless constructor to fix run migration by EF CLI that was broken

--- src/Hotel.Data/Contexts/HotelContext.cs
+++ src/Hotel.Data/Contexts/HotelContext.cs
@@ -12,6 +12,14 @@ namespace Hotel.Data.Contexts
     public class HotelContext : DbContext
     {
         #region Constructors
+        public HotelContext()
+        {
+            if (Database.GetPendingMigrations().Any())
+            {
+                Database.Migrate();
+            }
+        }
+
         public HotelContext(DbContextOptions<HotelContext> options) : base(options)
         {
             if (Database.GetPendingMigrations().Any())
